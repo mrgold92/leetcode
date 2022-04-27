e5af4b37e2f00c7547e567ac4936ed053f3d19fe
David Salazar <davids1_4@hotmail.com>
2022-04-27 22:54:00 +0200

    Nuevos cambios en main

--- main.py
+++ main.py
@@ -45,6 +45,12 @@ with open(pathRreadme, 'a') as f:
     f.write(table)
 
 
+# Subimos los archivos a GitHub
+os.system("git add .")
+os.system("git config user.name mrgold92")
+os.system("git config user.email davids1_4@hotmail.com")
+os.system("git commit -m '[bot] Actualizado README.md'")
+os.system("git push")
 
 
 
